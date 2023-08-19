c629eac6e9c06af54b7e40f64a092ecc62a416cf
Elana Tollu <elana.tollu@yandex.ru>
2023-08-20 00:42:31 +0300

    add db; add table cards

---
+++ web/migrations/2023-08-20-00-13-add-cards.mjs
@@ -0,0 +1,12 @@
+export const name = 'Add table cards'
+
+export async function transaction(sql){
+    await sql`
+        CREATE TABLE cards (
+            cards_id BIGSERIAL PRIMARY KEY,
+            cards_native TEXT NOT NULL,
+            cards_foreign TEXT NOT NULL,
+            cards_created_at TIMESTAMPTZ
+        )
+    `
+}
--- web/src/app/api/author/cards/route.ts
+++ web/src/app/api/author/cards/route.ts
@@ -1,11 +1,11 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
 
     return NextResponse.json({ now: new Date().toISOString() })
 }
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
     const newCard = await request.json();
     console.log('newCard', newCard);
 
